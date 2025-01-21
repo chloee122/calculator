3bb90253f191cbc9855ed486f87d95afa9769d56
Cinnamon Bun <nguyeney02@gmail.com>
2025-01-21 23:23:14 +0200

    Remove outdated code

--- src/utils/convertEuroCurrencyUnit.ts
+++ src/utils/convertEuroCurrencyUnit.ts
@@ -5,10 +5,6 @@ export const convertEuroToCent = (euro: number): number => {
     return euro * EURO_CENT_CONVERT_FACTOR;
 };
 
-export const convertCentToEuro = (cent: number): number => {
-    return cent / EURO_CENT_CONVERT_FACTOR;
-}
-
 export const convertCentToEuroString = (cent: number): string => {
     return (cent / EURO_CENT_CONVERT_FACTOR).toFixed(2);
 };
